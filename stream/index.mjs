4103460a350cdb135addc8c7c0bb25d2851278c3
eorthus <oakenshielddis@gmail.com>
2024-01-04 15:47:02 +0400

    #fix: add async iterator, highWaterMark

--- stream/index.mjs
+++ stream/index.mjs
@@ -3,36 +3,42 @@ import {
     createWriteStream,
 } from 'node:fs';
 
-
-const readableStream = createReadStream("input.txt");
-
-const writeableStream = createWriteStream("output.txt");
-
 const data = []
 
+//------------------------------------------------------------------------------------
+
 const sortingHandler = () => {
-    const dirtyArray = data?.map(el=> el.replaceAll('\r', ' ').replaceAll('\n', ' ').replaceAll(' ', ',').split(',').filter(el => el))
+    const dirtyArray = data?.map(el => el.replaceAll('\r', ' ').replaceAll('\n', ' ').replaceAll(' ', ',')).join('').split(',').filter(el => el)
     const sortingArray = dirtyArray?.flat().reduce((acc, el) => {
         acc[el] = (acc[el] || 0) + 1;
         return acc;
     }, {})
-
     return `[${Object.values(sortingArray)}]`
 }
 
-readableStream.on('data', (chunk) => {
-    data.push(chunk.toString())
-})
-
-
-readableStream.on('end', () => {
-
+const endHandler = async () => {
     try {
+        const writeableStream = createWriteStream("output.txt", { highWaterMark: 1 });
+
         writeableStream.write(sortingHandler())
+
+        writeableStream.end()
 
     } catch (err) {
         throw new Error(err)
-    } finally {
-        writeableStream.end()
     }
-})
+}
+
+//-------------------------------------------------------------------------------------
+
+try {
+    const readableStream = createReadStream("input.txt", { highWaterMark: 1 });
+
+    for await (const chunk of readableStream) {
+        data.push(chunk.toString())
+    }
+} catch (err) {
+    throw new Error(err)
+} finally {
+    endHandler()
+}
