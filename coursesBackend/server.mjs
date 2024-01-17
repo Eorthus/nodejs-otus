22f81270fa1727bfae74061ea3117666fbd11b32
eorthus <oakenshielddis@gmail.com>
2024-01-17 23:10:32 +0400

    #feat: mongodb

--- coursesBackend/server.mjs
+++ coursesBackend/server.mjs
@@ -1,21 +1,31 @@
-// Import the framework and instantiate it
 import Fastify from 'fastify'
 import Certificate from 'fastify-tls-keygen'
+import { MongoClient } from "mongodb";
+
+const dbUrl = 'mongodb://localhost:27017';
+
+const client = new MongoClient(dbUrl);
 
 const fastify = Fastify({
   logger: true,
   https: true,
 })
-  
+
 
 // Declare a route
 fastify.get('/', async function handler (request, reply) {
-  return { hello: 'world' }
+  const db = client.db('courses');
+  const collection = db.collection('courses');
+  const findResult = await collection.find({}).toArray();
+  return { hello: findResult }
 })
 
 // Run the server!
 try {
   await fastify.register(Certificate)
+
+  client.connect();
+
   fastify.listen({ port: 3000})
 } catch (err) {
   fastify.log.error(err)
