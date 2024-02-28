3de4eecd168f4ba5d8d96d263f3dbd2ca7c83e3a
eorthus <oakenshielddis@gmail.com>
2024-02-28 09:05:55 +0400

    #configure actual version

--- coursesBackend/src/app.ts
+++ coursesBackend/src/app.ts
@@ -17,8 +17,7 @@ app.use(cors())
 app.use(express.json())
 app.use(
     "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerJsdoc(swaggerOptions), { explorer: true })
+    swaggerUi.serveFiles(swaggerOptions), swaggerUi.setup(swaggerOptions)
   )
 app.use(passport.initialize()); 
 app.use('/api', userRouter)
